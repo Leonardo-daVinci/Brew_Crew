77ec8225a40ac0b421da516ff1279726a185436e
Akshit Keoliya <akshitkeoliya4@gmail.com>
2020-01-05 04:19:55 +0530

    Implemented BottomSheet

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/models/brew.dart';
 import 'package:brew_crew/screens/home/brewlist.dart';
+import 'package:brew_crew/screens/home/settingsform.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/services/database.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,16 @@ final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
+
+    void _showSettingsPanel(){
+      showModalBottomSheet(context:context, builder:(context){
+        return Container(
+          padding:EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+          child: SettingsForm(),
+        );
+      });
+    }
+
     return StreamProvider< List<Brew>>.value(
       value: DatabaseService().brews,
           child: Scaffold(
@@ -26,7 +37,12 @@ final AuthService _authService = AuthService();
                 await _authService.signOut();
               },
               label: Text('Logout'),
-            )
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.settings),
+              label: Text('Settings'),
+              onPressed: () => _showSettingsPanel(),
+              )
           ],
         ),
         body: BrewList(),
